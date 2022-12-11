57a3675d3c043a85dfbc623ffb332ef45f1125b1
josephdescallar <jdescallar17@gmail.com>
2022-12-12 00:00:53 +1100

    commit 10

---
+++ R/plot_phcsh_mpl.R
@@ -0,0 +1,172 @@
+plot_phcsh_mpl <- function(object,risk=1, plots = c("bh", "surv", "cif"),
+                  sand = FALSE, n.points = 1000){
+  r = risk
+  if("bh" %in% plots) bh = 1
+  else bh = 0
+  if("surv" %in% plots) surv = 1
+  else surv = 0
+  if("cif" %in% plots) cif = 1
+  else cif = 0
+  psif <- function(x, bknots, iknots){
+    splines2::mSpline(x, knots = iknots, Boundary = bknots, degree = object$dgr,
+            intercept = object$basis.intercept)
+  }
+  PSIf <- function(x, bknots, iknots){
+    splines2::iSpline(x, knots = iknots, Boundary = bknots, degree = object$dgr,
+            intercept = object$basis.intercept)
+  }
+  t.points = seq(object$b.knots[1], object$b.knots[2], length.out = n.points)
+  plot.psi = psif(t.points, object$b.knots, object$i.knots[[r]])
+  plot.h0r = plot.psi %*% object$"theta"[[r]]
+  plot.h0r[plot.h0r < 1e-6] <- 1e-6
+  if(sand == FALSE){
+    VarCovMat.theta = object$VarCovMat[object$theta.index[[r]],
+                      object$theta.index[[r]]]
+  }
+  else if(sand == TRUE){
+    VarCovMat.theta = object$sand[object$theta.index[[r]],
+                      object$theta.index[[r]]]
+  }
+  plot.h0r.var = diag(plot.psi %*% VarCovMat.theta %*% t(plot.psi))
+  if(object$pos.def==1){
+    plot.se = sqrt(plot.h0r.var) / (plot.h0r)
+    plot.h0r.lower = as.vector(plot.h0r) * exp(-1.96*plot.se)
+    plot.h0r.upper = as.vector(plot.h0r) * exp(1.96*plot.se)
+  }
+  #Baseline survival
+  plot.PSI = PSIf(t.points, object$b.knots, object$i.knots[[r]])
+  plot.S0r = exp(-(plot.PSI %*% object$"theta"[[r]]))
+  plot.S0r.logOR = log((1-plot.S0r) / plot.S0r)
+  plot.S0r.prime2 = plot.S0r^2
+  plot.chaz.var = diag(plot.PSI %*% VarCovMat.theta %*% t(plot.PSI))
+  plot.S0r.var = plot.S0r.prime2 * plot.chaz.var
+  plot.S0r.logOR.var = ((1/((1-plot.S0r)*plot.S0r))^2)*plot.S0r.var
+  if(object$pos.def==1){
+    plot.S0r.logOR.lower = plot.S0r.logOR + 1.96*sqrt(plot.S0r.logOR.var)
+    plot.S0r.logOR.upper = plot.S0r.logOR - 1.96*sqrt(plot.S0r.logOR.var)
+    plot.S0r.lower = exp(-plot.S0r.logOR.lower) / (1 +
+                     exp(-plot.S0r.logOR.lower))
+    plot.S0r.upper = exp(-plot.S0r.logOR.upper) / (1 +
+                     exp(-plot.S0r.logOR.upper))
+  }
+  #Cumulative incidence function
+  bma = (t.points - object$b.knots[1]) / 2
+  bpa = (t.points + object$b.knots[1]) / 2
+  t.gq.change = bma*matrix(object$nodes, nrow=length(bma), ncol =
+                object$gq.points, byrow=TRUE) + bpa
+  plot.F0r.psi.gq = plot.F0r.PSI.gq = plot.F0r.h0qt.gq = list()
+  plot.F0r.H0qt.gq = plot.F0r.S0qt.gq = plot.F0r.Integrand.gq = list()
+  plot.F0r.dhdT.gq = plot.F0r.dSdT.gq = plot.F0r.dFdT.gqT = list()
+  for(gq in 1:object$gq.points){
+    plot.F0r.psi.gq[[gq]] <- psif(t.gq.change[,gq], object$b.knots,
+                             object$i.knots[[r]])
+    plot.F0r.PSI.gq[[gq]] <- PSIf(t.gq.change[,gq], object$b.knots,
+                             object$i.knots[[r]])
+    plot.F0r.h0qt.gq[[gq]] <- psif(t.gq.change[,gq], object$b.knots,
+                              object$i.knots[[r]]) %*% object$"theta"[[r]]
+    plot.F0r.H0qt.gq.r = plot.F0r.S0qt.gq.r = list()
+    for(q in 1:object$n.risk){
+      plot.F0r.H0qt.gq.r[[q]] = PSIf(t.gq.change[,gq], object$b.knots,
+                                object$i.knots[[q]]) %*% object$"theta"[[q]]
+      plot.F0r.S0qt.gq.r[[q]] = exp(-plot.F0r.H0qt.gq.r[[q]])
+    }
+    plot.F0r.S0qt.gq[[gq]] = Reduce("*", plot.F0r.S0qt.gq.r)
+    plot.F0r.Integrand.gq[[gq]] <- object$weights[gq]*plot.F0r.h0qt.gq[[gq]] *
+    plot.F0r.S0qt.gq[[gq]]
+    plot.F0r.dhdT.gq[[gq]] <- plot.F0r.psi.gq[[gq]]
+    plot.F0r.dSdT.gq[[gq]] <- -plot.F0r.PSI.gq[[gq]] *
+                              as.vector(plot.F0r.S0qt.gq[[gq]])
+    plot.F0r.dFdT.gqT[[gq]] <- as.matrix(plot.F0r.dhdT.gq[[gq]] *
+                               as.vector(plot.F0r.S0qt.gq[[gq]]) +
+                               as.vector(plot.F0r.h0qt.gq[[gq]]) *
+                               as.matrix(plot.F0r.dSdT.gq[[gq]]))
+  }
+  plot.F0r <- bma*Reduce("+",plot.F0r.Integrand.gq)
+  plot.F0r.dFdT <- bma*Reduce("+",plot.F0r.dFdT.gqT)
+  plot.F0r.r.var <- diag(plot.F0r.dFdT %*% as.matrix(VarCovMat.theta) %*%
+                    t(plot.F0r.dFdT))
+  plot.F0r.logOR <- log(plot.F0r / (1-plot.F0r + 1e-12) + 1e-12)
+  plot.F0r.logOR.var = ((1/((1-plot.F0r)*plot.F0r))^2)*plot.F0r.r.var
+  plot.F0r.log <- log(plot.F0r + 1e-12)
+  plot.F0r.log.var <- (1/plot.F0r^2)*plot.F0r.r.var
+  if(object$pos.def==1){
+    # plot.F0r.logOR.lower = plot.F0r.logOR - 1.96*sqrt(plot.F0r.logOR.var)
+    # plot.F0r.logOR.upper = plot.F0r.logOR + 1.96*sqrt(plot.F0r.logOR.var)
+    # plot.F0r.lower <- exp(plot.F0r.logOR.lower) / (1 + exp(plot.F0r.logOR.lower))
+    # plot.F0r.upper <- exp(plot.F0r.logOR.upper) / (1 + exp(plot.F0r.logOR.upper))
+    plot.F0r.lower <- plot.F0r - 1.96*sqrt(plot.F0r.r.var)
+    plot.F0r.upper <- plot.F0r + 1.96*sqrt(plot.F0r.r.var)
+    #plot.F0r.log.lower <-  plot.F0r.log - 1.96*sqrt(plot.F0r.log.var)
+    #plot.F0r.log.upper <-  plot.F0r.log + 1.96*sqrt(plot.F0r.log.var)
+    #plot.F0r.lower <- exp(plot.F0r.log.lower)
+    #plot.F0r.upper <- exp(plot.F0r.log.upper)
+  }
+  plot.bh = function(h0,low=NULL,up=NULL){
+    if(object$pos.def==1){
+      plot(t.points, h0, xlab = "t", main = paste("Risk", risk),
+      ylab = "baseline hazard", ylim = c(min(low, na.rm = TRUE),
+      max(plot.h0r.upper, na.rm = TRUE)), type = "l")
+      graphics::lines(t.points, low, lty = "dashed")
+      graphics::lines(t.points, up, lty = "dashed")
+    }
+    else{
+      plot(t.points, h0, xlab = "t", main = paste("Risk", risk), ylab =
+      "baseline hazard", ylim = c(min(h0, na.rm = TRUE), max(h0, na.rm = TRUE)),
+      type = "l")
+    }
+  }
+  plot.surv = function(S0r,low=NULL,up=NULL){
+    title = if(bh==0) paste("Risk", risk)
+    else ""
+    plot(t.points, S0r, xlab = "t", ylab = "Survival", ylim = c(0,1),
+         main = title, type = "l")
+    if(object$pos.def==1){
+      graphics::lines(t.points, low, lty = "dashed")
+      graphics::lines(t.points, up, lty = "dashed")
+    }
+  }
+  plot.cif = function(F0r, low=NULL, up=NULL){
+    title = if(surv==0 & bh == 0) paste("Risk", risk)
+    else ""
+    plot(t.points, F0r, xlab = "t", ylab = "CIF", ylim = c(-0.5,1),
+    main = title, type = "l")
+    if(object$pos.def==1){
+      graphics::lines(t.points, low, lty = "dashed")
+      graphics::lines(t.points, up, lty = "dashed")
+    }
+  }
+  graphics::par(mfrow = c(sum(bh, surv, cif),1))
+  if(bh == 1) {
+    if(object$pos.def==1)  plot.bh(plot.h0r, plot.h0r.lower, plot.h0r.upper)
+    else plot.bh(plot.h0r)
+  }
+  if(surv == 1){
+    if(object$pos.def==1) plot.surv(plot.S0r,plot.S0r.lower,plot.S0r.upper)
+    else plot.surv(plot.S0r)
+  }
+  if(cif == 1){
+    if(object$pos.def==1) plot.cif(plot.F0r, plot.F0r.lower, plot.F0r.upper)
+    else plot.cif(plot.F0r)
+  }
+  rlist <- list("plot.F0r.r.var"=plot.F0r.r.var,
+           "plot.F0r.lower"=plot.F0r.lower, "plot.S0r.var"=plot.S0r.var,
+           "plot.S0r.logOR.var"=plot.S0r.logOR.var,
+           "plot.F0r.logOR.var"=plot.F0r.logOR.var)
+  return(rlist)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ R/summary_phcsh_mpl.R
@@ -0,0 +1,36 @@
+summary_phcsh_mpl <- function(object, sand = FALSE){
+  if(sand == FALSE){
+    col.names = c("Estimate", "Std. Error", "z-value", "Pr(>|z|)",
+                "Gradient")
+  }
+  else if(sand == TRUE){
+    col.names = c("Estimate", "Std. Error (sandwich)", "z-value", "Pr(>|z|)",
+                "Gradient")
+  }
+  var.names = dimnames(object$data$X)[[2]]
+  risks = list()
+  for(r in 1:object$n.risk){
+    if(sand == FALSE){
+      matxB = cbind(object$"beta"[[r]],object$seB[[r]], object$"beta"[[r]] /
+              object$seB[[r]],2*(1-stats::pnorm(abs(object$"beta"[[r]] /
+              object$seB[[r]]))), object$beta.gradient[[r]])
+      matxT = cbind(object$"theta"[[r]], object$seT[[r]], object$"theta"[[r]] /
+              object$seT[[r]],2*(1-stats::pnorm(abs(object$"theta"[[r]] /
+              object$seT[[r]]))), object$theta.gradient[[r]])
+    }
+    else if(sand == TRUE){
+      matxB = cbind(object$"beta"[[r]],object$seB.sand[[r]], object$"beta"[[r]]
+              / object$seB.sand[[r]],2*(1-stats::pnorm(abs(object$"beta"[[r]] /
+              object$seB.sand[[r]]))), object$beta.gradient[[r]])
+      matxT = cbind(object$"theta"[[r]], object$seT.sand[[r]],
+              object$"theta"[[r]] / object$seT.sand[[r]],2 *
+              (1-stats::pnorm(abs(object$"theta"[[r]] / object$seT.sand[[r]]))),
+              object$theta.gradient[[r]])
+    }
+    dimnames(matxB) = list(paste(" ", var.names, sep = ""),col.names)
+    colnames(matxT) = col.names
+    risks[[r]] = list(Beta = matxB, Theta = matxT)
+  }
+  out <- list(risks=risks)
+  out
+}
