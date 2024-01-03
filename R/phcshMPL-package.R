552bc05f12b27e6c5bd585f98f43c22dee524d0e
Joseph Descallar <jdescallar17@gmail.com>
2024-01-03 17:34:23 +1100

    description

--- R/phcshMPL-package.R
+++ R/phcshMPL-package.R
@@ -2,5 +2,8 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' Simultaneously estimate the regression coefficients and provide a
+#' ’non-parametric’ smooth estimate of the baseline hazard function for
+#' proportional hazard Cox models using maximum penalised likelihood (MPL).
 ## usethis namespace: end
 NULL
--- R/phcsh_mpl.R
+++ R/phcsh_mpl.R
@@ -1,19 +1,26 @@
-#' Fit Cause Specific Proportional Hazards Regression Model
-#'  Via MPL
+#' Fit Cause Specific Proportional Hazards Regression Model Via MPL
 #'
 #' Simultaneously estimate the regression coefficients and the baseline hazard
 #' function of proportional hazard Cause specific hazards models for competing
-#' risks with a cure fraction for at least one risk using maximum penalised
-#' likelihood (MPL).
+#' risks with or without a cure fraction using maximum penalized likelihood
+#' (MPL).
 #'
 #' @param formula a formula object, with the response on the left of a ~
 #' operator, and the terms on the right. The response must be a survival object
 #' as returned by the Surv function (in the Surv, use type = "interval" with
 #' interval censored data).
 #'
-#' @param risk test
-#' @param z test
-#' @param data test
+#' @param risk a numeric vector which contains the risk/failure for the
+#' i-th observation in the data frame.
+#'
+#' @param z an optional data frame which contains the covariate to be used when
+#' a cure fraction is specified. If left NULL, then no cure fraction is assumed,
+#' otherwise parameter estimation will occur for a cure fraction based on
+#' the specified variables.
+#'
+#' @param data a data frame that includes the variables named in the formula
+#' argument
+#'
 #' @param control test
 #' @param ... Other arguments. In coxph_mpl, these elements, will be passed to
 #' coxph_mpl_control. In print.coxph_mpl, these elements will be passed to the
