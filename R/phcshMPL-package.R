5e17cfb866795f286f7d16d6ee5dbef5e4c3a8bd
Joseph Descallar <jdescallar17@gmail.com>
2024-01-03 17:36:33 +1100

    description

--- R/phcshMPL-package.R
+++ R/phcshMPL-package.R
@@ -2,7 +2,7 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' Simultaneously estimate the regression coefficients and provide a
+#' @description Simultaneously estimate the regression coefficients and provide a
 #' ’non-parametric’ smooth estimate of the baseline hazard function for
 #' proportional hazard Cox models using maximum penalised likelihood (MPL).
 ## usethis namespace: end
