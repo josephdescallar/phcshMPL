938f79ee1776d7c4212b1df2f8b3a44823ec40c4
Joseph Descallar <jdescallar17@gmail.com>
2024-01-03 18:10:57 +1100

    desc

--- R/phcshMPL-package.R
+++ R/phcshMPL-package.R
@@ -1,4 +1,15 @@
-#' test the title of the function package page
+#' test me titel
 #'
-#' test the description of the function
+#' @description
+#' Estimate the regression coefficients and the baseline hazard of proportional
+#' hazard Cox models for competing risks data with left, right or interval
+#' censored survival data using maximum penalised likelihood.
+#' A 'non-parametric' smooth estimate of the baseline hazard function is
+#' provided test 2
+#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
