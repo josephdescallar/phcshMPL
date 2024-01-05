3656b097293853d9b014a6391109e8d92277eb78
Joseph Descallar <jdescallar17@gmail.com>
2024-01-06 01:27:52 +1100

    data frame

--- R/predict.phcsh_mpl.R
+++ R/predict.phcsh_mpl.R
@@ -137,11 +137,11 @@ predict.phcsh_mpl <- function(object, covs=NULL, risk=1, n.points=1000,
     pred.F0r.upper <- exp(pred.F0r.logOR.upper) /
     (1 + exp(pred.F0r.logOR.upper))
   }
-  predValues <- cbind("t.points"=t.points, "pred.h0r"=pred.h0r,
+  predValues <- data.frame(cbind("t.points"=t.points, "pred.h0r"=pred.h0r,
   "pred.h0r.lower"=pred.h0r.lower, "pred.h0r.upper"=pred.h0r.upper,
   "pred.S0r"=pred.S0r, "pred.S0r.lower"=pred.S0r.lower,
   "pred.S0r.upper"=pred.S0r.upper, "pred.F0r"=pred.F0r,
-  "pred.F0r.lower"=pred.F0r.lower, "pred.F0r.upper"=pred.F0r.upper)
+  "pred.F0r.lower"=pred.F0r.lower, "pred.F0r.upper"=pred.F0r.upper))
   colnames(predValues) <- c("t", "pred.h0r", "pred.h0r.lower", "pred.h0r.upper",
   "pred.S0r", "pred.S0r.lower", "pred.S0r.upper", "pred.F0r", "pred.F0r.lower",
   "pred.F0r.upper")
